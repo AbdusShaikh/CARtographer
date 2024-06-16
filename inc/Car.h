291605d499dd479c834dad4d67f898af9f06eefd
abdus <abdus.shaikh123@gmail.com>
2024-06-16 16:13:09 -0400

    New pin setup. Now supports both forward and backwards movement

--- inc/Car.h
+++ inc/Car.h
@@ -1,6 +1,7 @@
 #include <pigpio.h>
 #include "common.h"
 
+
 class Car{
     public:
         Car();
@@ -13,8 +14,11 @@ class Car{
         void main();
     private:
         int m_enaPin = 21;
-        int m_inLeftWheelPin = 20; // in1
-        int m_rightWheelPin = 16; // in2
+        int m_enbPin = 26;
+        int m_leftWheelForwardPin= 19;
+        int m_leftWheelBackwardPin = 13;
+        int m_rightWheelBackwardPin = 20; 
+        int m_leftWheelBackwardPin = 16; 
         int m_dutyCycle = 255;
 
         iFace* m_interface;
